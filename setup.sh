954491be4c7565f6edfa3ddb60f5b497208c0d80
osamingo <osamingo@gmail.com>
2014-06-24 22:05:32 +0900

    Edit setup.sh

--- setup.sh
+++ setup.sh
@@ -17,14 +17,12 @@ killall Finder Dock
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
-# Install homesick
-gem install homesick
-homesick symlink dotfiles
-
 # Kick Brewfile
 brew bundle ~/dotfiles/Brewfile
 
 # Install oh-my-zsh
 curl -L http://install.ohmyz.sh | sh
 
+# Init template directory
+git config --global init.templatedir '~/.git_template'
 
